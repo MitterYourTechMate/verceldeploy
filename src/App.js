be2df43f45da42ba22ce5eb982bba6b3a4c078f3
shankey raheja <shankeyraheja@gmail.com>
2024-04-26 19:21:04 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HELLO WORLD</h1>
+      <h1>THIS IS MY FIRST REACT APP</h1>
+      <h1>DEPLOY ME ON VERCEL</h1>
     </div>
   );
 }
